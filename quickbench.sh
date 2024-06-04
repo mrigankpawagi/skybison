a00b4d26453e389e05267f351f133335183c59d9
Mrigank Pawagi <mrigankpawagi@gmail.com>
2024-06-04 20:02:39 +0530

    update script

--- quickbench.sh
+++ quickbench.sh
@@ -1,15 +1,19 @@
 sudo apt update
 sudo apt install -y ninja-build ccache valgrind
 
-cmake -GNinja -B /workspaces/skybison/build \
+# sudo apt install libstdc++-12-dev libbz2-dev
+
+cmake -GNinja -B build \
         -DCMAKE_TOOLCHAIN_FILE=util/linux.cmake \
         -DCMAKE_BUILD_TYPE=DebugOpt \
-        -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+        -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ \
+        # -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON
 
-cd /workspaces/skybison/build && ninja -j4
+cd build && ninja -j4
 
 mkdir -p /var/tmp/django_minimal
 pushd /var/tmp/django_minimal
-/workspaces/skybison/benchmarks/benchmarks/django/setup_django_minimal.sh
+# $1 is the path to the skybison directory
+$1/benchmarks/benchmarks/django/setup_django_minimal.sh
 popd
-cd /workspaces/skybison && ./benchmarks/quickbench/diffrevs.py --run-django HEAD^ HEAD | tee django-benchmark-results.json
+cd $1 && ./benchmarks/benchmarks/django/benchmark.py --django-dir /var/tmp/django_minimal/ --interpreter ~/static-python/cinder/python --callgrind --callgrind-out-dir $(pwd) --num-requests 10
